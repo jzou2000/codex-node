fa6957cc8320f7ed32a1e12450781bcf2f4f045c
Jason Zou <jasonz@simba.com>
2017-04-12 15:23:18 -0700

    show param usage in REST

--- ws.js
+++ ws.js
@@ -8,6 +8,13 @@ app.get('/rest', function(req, res) {
     res.send('hello world')
 })
 
+app.get('/users/:uid/task/:taskid', function(req, res) {
+    // give parameters in REST
+    // for /user/jason/task/build, the output will be
+    // {"uid":"jason","taskid":"build"}
+    res.send(req.params)
+})
+
 var server = app.listen(8888, function() {
     var host = server.address().address
     var port = server.address().port
