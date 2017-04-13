4241d0bf334d28b9904b3a9fc4b6be2885bd3dcf
Jason Zou <jasonz@simba.com>
2017-04-12 18:31:44 -0700

    add getProverb() in ws.js, iterate each proverb on url /rest

--- ws.js
+++ ws.js
@@ -3,10 +3,42 @@ var app = express()
 
 app.use(express.static('public'))
 
+var fs = require('fs')
+function getProverb(fname)
+{
+    proverb = [];
+    fs.readFile(fname, function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        lines = data.toString().split('\n');
+        var pat = /\s*"\s*(.*?)\s*"\s*/;
+        var p;
+        for (i in lines) {
+            m = pat.exec(lines[i]);
+            if (m == null) {
+                if (p != undefined) p.desc += lines[i];
+            } else {
+                if (p != undefined) proverb.push(p);
+                p = { text: m[1], desc: '' };
+            }
+        }
+        if (p != undefined) proverb.push(p);
+        console.log('loaded: %s', fname);
+    });
+    return proverb;
+}
+
+
+ps = { proverb: getProverb('proverb50.txt'),
+       index: 0 };
 
 app.get('/rest', function(req, res) {
-    res.send('hello world')
-})
+    var t = ps.proverb[ps.index];
+    if (ps.index++ >= ps.proverb.length)
+        ps.index = 0;
+    res.send('<h2>' + t.text + '</h2>' + '<p>' + t.desc + '</p>')
+});
 
 app.get('/users/:uid/task/:taskid', function(req, res) {
     // give parameters in REST
